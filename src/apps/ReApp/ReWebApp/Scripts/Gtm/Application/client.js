ab871c83c7c0920d636a62602a2ada36eeb0cbc1
tgrinyak <tgrinyak@gmail.com>
2014-12-30 05:29:15 -0800

    next preparation step - jquery, first calls from view

---
+++ src/apps/ReApp/ReWebApp/Scripts/Gtm/Application/client.js
@@ -0,0 +1,20 @@
+﻿var Gtm;
+(function (Gtm) {
+    (function (Application) {
+
+        var Client = (function () {
+
+            function Client() {
+            }
+
+            Client.init = function () {
+                console.log("test from Gtm.Application.Client.init()");
+            };
+
+            return Client;
+        })();
+        Application.Client = Client;
+
+    })(Gtm.Application || (Gtm.Application = {}));
+    var Application = Gtm.Application;
+})(Gtm || (Gtm = {}));
