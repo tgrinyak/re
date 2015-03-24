0cb3c78cff3b28c4268ab4f76944b0c248646c2a
tgrinyak <tgrinyak@gmail.com>
2015-03-24 01:25:01 -0700

    getting starting with adding angularjs

---
+++ src/apps/ReApp/ReWebApp/Scripts/Gtm/app.js
@@ -0,0 +1,23 @@
+﻿var Gtm;
+(function (Gtm) {
+    (function (app) {
+        app.controller("appController", ["$scope", function ($scope) {
+            var app = {};
+
+            (function () {
+                app.visiblePage = "login";
+
+                $scope.app = app;
+            })();
+
+        }]);
+
+        app.controller("loginController", ["$scope", function ($scope) {
+            console.log("loginController");
+        }]);
+
+        // angular initialization
+        // ..
+    })(Gtm.app || (Gtm.app = angular.module("Gtm.app", [])));
+    var app = Gtm.app;
+})(Gtm || (Gtm = {}));
